f4bfc7e7e7bf6a6a58ef56f5a2a4525a1d3f199b
OtavioMendes12 <otaviojulio.mendes@gmail.com>
2024-02-21 16:00:41 -0300

    Ajustes

--- menu.js
+++ menu.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header-movimento')
     header.classList.toggle('rolagem',window.scrollY > 30)
 })
+
+document.addEventListener("mousemove", function(e) {
+  
+    var rocket = document.querySelector(".rocket");
+    rocket.style.left = e.offsetX + 'px';
+    rocket.style.top = e.offsetY + 'px';
+    
+  });
