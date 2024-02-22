04da1b42062f8678816a4bac9ea9bd789f8d3618
OtavioMendes12 <otaviojulio.mendes@gmail.com>
2024-02-22 13:54:36 -0300

    Update

--- menu.js
+++ menu.js
@@ -3,10 +3,3 @@ window.addEventListener("scroll", function(){
     header.classList.toggle('rolagem',window.scrollY > 30)
 })
 
-document.addEventListener("mousemove", function(e) {
-  
-    var rocket = document.querySelector(".rocket");
-    rocket.style.left = e.offsetX + 'px';
-    rocket.style.top = e.offsetY + 'px';
-    
-  });
