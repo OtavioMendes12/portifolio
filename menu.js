1e94a78d971b0efd468bd0d573a6548111adf7c5
OtavioMendes12 <otaviojulio.mendes@gmail.com>
2024-02-27 15:52:39 -0300

    update

--- menu.js
+++ menu.js
@@ -3,3 +3,12 @@ window.addEventListener("scroll", function(){
     header.classList.toggle('rolagem',window.scrollY > 30)
 })
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
